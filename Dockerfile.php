3afd45fa5242962f450cc4c9281ca4bfe95450bf
Adem Sinankili <adem.sinankili@gmail.com>
2023-11-13 11:56:36 +0100

    Fix dockerfiles

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,10 +1,10 @@
 FROM php:8.2-fpm-alpine
-COPY . /var/www/html/
+COPY . /srv/
 
 RUN apk add --no-cache git libzip-dev zip \
     && docker-php-ext-install zip \
-    && cd /var/www/html \
+    && cd /srv \
     && chmod +x composer_install.sh && ./composer_install.sh \ 
     && mv composer.phar /usr/local/bin/composer \
     && composer install \
-    && rm composer_install.sh Dockerfile.caddy Dockerfile.php 
+    && rm composer_install.sh Dockerfile.caddy Dockerfile.php
